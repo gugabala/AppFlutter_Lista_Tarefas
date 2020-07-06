cc251675aa9af3ca7d60b906eb3b2943df7ef42d
gugabala <gugabala@gmail.com>
2020-07-06 16:02:12 -0300

    SnackBar e Desfazendo Ação

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,9 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -113,22 +116,30 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+          final snack = SnackBar(
+            content: Text("Tarefa \'${_lastRemoved["title"]}\' removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 3),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
-
-  /*CheckboxListTile(
-  title: Text(_toDoList[index]["title"]),
-  value: _toDoList[index]["ok"],
-  secondary: CircleAvatar(
-  child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-  ),
-  onChanged: (c) {
-  setState(() {
-  _toDoList[index]["ok"] = c;
-  _saveData();
-  });
-  },
-  );*/
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
